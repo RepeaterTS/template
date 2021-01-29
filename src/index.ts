30c009a98eec45b8e0374f07e9a0f486407d21af
Jordan Jones <jordpjones@gmail.com>
2021-01-28 23:49:08 -0800

    + jest for testing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-/**
- * The mythical function
- */
-export default function foo(): string {
-	return 'bar';
-}
+export const main = () => 'kashall is bad at typescript';
+
+export default main;
--- test/Util.ts
+++
@@ -1,6 +0,0 @@
-import ava from 'ava';
-import util from '../src';
-
-ava('sample test', (test): void => {
-	test.is(util(), 'bar');
-});
